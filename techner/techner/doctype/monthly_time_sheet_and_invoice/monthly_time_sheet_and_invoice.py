cabc9ef67b3346f249c0f7a75b06e77315845288
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-04-04 13:30:03 +0000

    fect:cancelled to cancel

--- techner/techner/doctype/monthly_time_sheet_and_invoice/monthly_time_sheet_and_invoice.py
+++ techner/techner/doctype/monthly_time_sheet_and_invoice/monthly_time_sheet_and_invoice.py
@@ -8,9 +8,9 @@ from frappe.model.document import Document
 class MonthlyTimeSheetAndInvoice(Document):
 	def before_save(self):
 		if frappe.db.exists("Monthly Time Sheet And Invoice", {"employee": self.employee,"month":self.month,"docstatus":1}):
-			frappe.throw(_("{0} TimeSheet and Invoice is submitted already. First Cancelled existing record then save new record").format(self.month))
+			frappe.throw(_("{0} TimeSheet and Invoice is submitted already. First Cancel existing record then save new record").format(self.month))
 
 	def before_submit(self):
 		if frappe.db.exists("Monthly Time Sheet And Invoice", {"employee": self.employee,"month":self.month,"docstatus":1}):
-			frappe.throw(_("{0} TimeSheet and Invoice is submitted already. First Cancelled existing record then save new record").format(self.month))
+			frappe.throw(_("{0} TimeSheet and Invoice is submitted already. First Cancel existing record then save new record").format(self.month))
 
