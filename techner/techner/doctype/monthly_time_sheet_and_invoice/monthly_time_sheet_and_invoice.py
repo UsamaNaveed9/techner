deb8b42fecbed2fc077e7cfc836ced765e5aae63
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-04-04 13:22:01 +0000

    fect:user not allowed to submit second timesheet of same month

--- techner/techner/doctype/monthly_time_sheet_and_invoice/monthly_time_sheet_and_invoice.py
+++ techner/techner/doctype/monthly_time_sheet_and_invoice/monthly_time_sheet_and_invoice.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2023, Techner and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _, msgprint
 from frappe.model.document import Document
 
 class MonthlyTimeSheetAndInvoice(Document):
-	pass
+	def before_save(self):
+		if frappe.db.exists("Monthly Time Sheet And Invoice", {"employee": self.employee,"month":self.month,"docstatus":1}):
+			frappe.throw(_("{0} TimeSheet and Invoice is submitted already. First Cancelled existing record then save new record").format(self.month))
+
+	def before_submit(self):
+		if frappe.db.exists("Monthly Time Sheet And Invoice", {"employee": self.employee,"month":self.month,"docstatus":1}):
+			frappe.throw(_("{0} TimeSheet and Invoice is submitted already. First Cancelled existing record then save new record").format(self.month))
+
